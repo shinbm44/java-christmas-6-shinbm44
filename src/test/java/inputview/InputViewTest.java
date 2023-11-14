89a55b12d3f05931ed56df45ae149edf5c390e82
hyunjin_byun <shinbm219@naver.com>
2023-11-14 13:38:34 +0900

    test:날짜입력테스트코드추가

---
+++ src/test/java/inputview/InputViewTest.java
@@ -0,0 +1,57 @@
+package inputview;
+
+import org.assertj.core.api.ThrowableAssert;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class InputViewTest {
+
+    @DisplayName("입력받은 날짜에 대한 확인 메소드 호출")
+    @Test
+    void readDate() {
+        String Date1 = "5일";
+        checkInt(Date1);
+
+        String Date2 = "32";
+        checkDateRange(Date2);
+
+    }
+
+    @DisplayName("1~31일의 범주 숫자로 날짜 입력이 들어왔는가?")
+    @Test
+    private void checkDateRange(String date2) {
+        int date = Integer.parseInt(date2);
+        assertThatThrownBy(() -> {
+            if (date > 31 || date < 1) {
+                throw new IllegalArgumentException("1~31일 기간에 포함되는 날짜를 입력해주세요");
+            }
+        })
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("1~31일 기간에 포함되는 날짜를 입력해주세요");
+    }
+
+
+    @DisplayName("입력받은 날짜가 정수인지 확인")
+    @Test
+    private void checkInt(String date1) {
+        assertThatThrownBy(()->{
+            if( !isInteger(date1)) {
+                throw new IllegalArgumentException("날짜는 정수만 입력해주세요");
+            }
+        })
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("날짜는 정수만 입력해주세요");
+    }
+
+    private boolean isInteger(String date1) {
+        try {
+            int data = Integer.parseInt(date1);
+            return true;
+        }catch (NumberFormatException e){
+            return false;
+        }
+    }
+
+}
