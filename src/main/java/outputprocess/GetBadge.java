c7c2199cfb1beef0f15569b11f911b37a0d56d1a
hyunjin_byun <shinbm219@naver.com>
2023-11-15 23:45:16 +0900

    이모지제거

--- src/main/java/outputprocess/GetBadge.java
+++ src/main/java/outputprocess/GetBadge.java
@@ -9,15 +9,15 @@ public class GetBadge {
 
     private void checkBadge(int discountMoney) {
         if(discountMoney > 20000) {
-            System.out.println("산타\uD83C\uDF85");
+            System.out.println("산타");
             return;
         }
         if(discountMoney > 10000) {
-            System.out.println("트리\uD83C\uDF84");
+            System.out.println("트리");
             return;
         }
         if(discountMoney > 5000) {
-            System.out.println("별⭐");
+            System.out.println("별");
             return;
         }
         System.out.println("없음");
